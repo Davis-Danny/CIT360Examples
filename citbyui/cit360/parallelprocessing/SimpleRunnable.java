f4e70dd90c9561feb2de0bc9015e0039421e0384
Davis-Danny <ddude1282@gmail.com>
2015-05-06 11:18:35 -0600

    update

--- citbyui/cit360/parallelprocessing/SimpleRunnable.java
+++ citbyui/cit360/parallelprocessing/SimpleRunnable.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package citbyui.cit360.parallelprocessing;
 
-/**
- *
- * @author buzzr
- */
-public class SimpleRunnable {
+
+public class SimpleRunnable implements Runnable{
     int pathNum;
     
     public SimpleRunnable(){
